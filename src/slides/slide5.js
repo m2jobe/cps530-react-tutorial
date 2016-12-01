d035ca847e5e2cb2e59972514bf192f611c97925
Bill Sheikh <bsheikh@adparlor.com>
2016-12-01 02:02:02 -0500

    finished demo

--- src/slides/slide5.js
+++ src/slides/slide5.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import store from '../store'
 
-export default class Slide5 {
+export default class Slide5 extends React.Component {
+
+  componentDidMount() {
+    this.timer = setInterval( (() => {
+      this.tick();
+    }), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    store.dispatch({type: 'tick', offset: 1})
+  }
 
   render() {
     return(
-      <div className="slide">
-        <h1>slide 4</h1>
+      <div className='title slide'>
+        <h1>{this.props.count} <img className={this.props.count % 2 == 0 ? "redux-logo" : "hide"} src="https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png" /></h1>
       </div>
     );
   }
